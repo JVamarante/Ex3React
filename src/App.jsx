058994df25fefb539f4215f7a434f768c3db14d5
JVamarante <joaovamarante@gmail.com>
2025-04-25 12:18:43 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
-import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
-import EstaComSono from './components/EstaComSono'
 import Aluno from './components/Aluno'
+
 const App = () => {
+  const alunos = [
+    { nome: "João", email: "joao@gmail.com", curso: "Sistemas de Internet", media: 8.5 },
+    { nome: "Maria", email: "maria@gmail.com", curso: "Sistemas de Internet", media: 6.8 },
+    { nome: "Kellyanne", email: "kellyanne@gmail.com", curso: "Sistemas de Informação", media: 7.0 },
+  ]
+
   return (
     <div>
-    {
-      [ {nome: "João", email: "joao@gmail.com", curso: "Sistemas de Internet"},
-        {nome: "Maria", email: "Maria@gmail.com", curso: "Sistemas de Internet"},
-        {nome: "Kellyanne", email: "kellyanne@gmail.com", curso: "Sistemas de Informação"},
-      ].map((aluno) => 
-        <Aluno 
-          nome={aluno.nome}
-          email={aluno.email}
-          curso={aluno.curso}
-        />  
-      )
-    }
+      {alunos.map((aluno, index) => (
+        <Aluno key={index} {...aluno} />
+      ))}
     </div>
   )
 }
--- src/components/Aluno.jsx
+++ src/components/Aluno.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const Aluno = ({ nome, email, curso }) => {
+const Aluno = ({ nome, email, curso, media }) => {
+  const status = media >= 7 ? 'APROVADO(A)' : 'REPROVADO(A)'
+
   return (
-    <div>
-        <p>Nome:{nome}</p>
-        <p>Email:{email}</p>
-        <p>Curso:{curso}</p>
-        <p>-------------------</p>
-    </div>
+    <>
+      <p>Nome: {nome}</p>
+      <p>Email: {email}</p>
+      <p>Curso: {curso}</p>
+      <p>Média: {media}</p>
+      <p>Status: {status}</p>
+      <p>-------------------</p>
+    </>
   )
 }
 
